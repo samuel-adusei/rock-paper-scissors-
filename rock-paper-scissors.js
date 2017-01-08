dfbe728b4f4c10334ac7a38cd519109341e1c101
samuel-adusei <samuel-adusei@hotmail.com>
2017-01-08 02:48:46 +0000

    final

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,6 +1,6 @@
-var userChoice = prompt("Do you choose rock,paper or scissors?")
+var userChoice = prompt("Do you choose rock,paper or scissors?")//  created a variable which will display the string
 
-var computerChoice = Math.random()
+var computerChoice = Math.random()//created a variable with Math.random() which will choose random number from 0 to 1 including floats
 console.log(computerChoice);
 
 if (computerChoice < 0.34) {
@@ -12,8 +12,10 @@ else if (computerChoice < 0.67) {
 else {
 	computerChoice = "scissors";
 }
+// created a conditonal which decides for the computer using the random
 
-var compare = function(choice1 == choice2)	{
+
+var compare = function(userChoice,computerChoice)	{
 	if (choice1 == choice2)
 		return "this result is a tie";
 	
@@ -27,3 +29,5 @@ var compare = function(choice1 == choice2)	{
 		}
 	}
 };
+
+//created another conditonional which when you choose one of the option it returns another.
